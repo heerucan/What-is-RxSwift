d6b0f6d0335edfef6df917b1a530381b15d06c5c
heerucan <iwpow0307@gmail.com>
2021-09-24 00:02:35 +0900

    [add] Observable 연습

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🤮 What The... RxSwift 톺아보자
+# ⚠️ What The... RxSwift 톺아보자
 
 
 #### 👷👷‍♀️👷 공부 정리는 벽돌 클릭... [🧱 ](https://elastic-failing-242.notion.site/fd0027e5ba5a43feb5faebcf4d0e09f1?v=08324b9d28f04128ae5cf453bf8ac9d5)
@@ -9,7 +9,7 @@
 |:-|:-|:-:|
 |BearFried + RxSwift|곰튀김 4시간 시즌2|[🐻](https://github.com/iamchiwon/RxSwift_In_4_Hours)|
 |RxSwift-Tutorial|iOS academy 유튜브|[🦠](https://www.youtube.com/watch?v=ES5RuLSv61g&feature=emb_imp_woyt)|
-|RxSwift.playground| 그냥 연습파일 | - |
+|Theory| 그냥 연습파일 | [🌼](https://elastic-failing-242.notion.site/fd0027e5ba5a43feb5faebcf4d0e09f1?v=08324b9d28f04128ae5cf453bf8ac9d5) |
 
 <br>
 <br>
---
+++ Theory/Theory/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Theory
+//
+//  Created by Thisisme Hi on 2021/09/23.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+    
+    private var disposeBag = DisposeBag()
+    var disposables: [Disposable] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        observable()
+    }
+    
+    func observable() {
+        // create
+        print("create-----------------")
+        Observable<Int>.create { (observer) -> Disposable in
+            observer.onNext(2)
+            observer.onNext(3)
+            observer.onCompleted()
+            
+            return Disposables.create()
+        }.subscribe(onNext: { event in
+            print(event)
+        }).disposed(by: disposeBag)
+
+        
+        // just
+        print("just-----------------")
+        let justObservable = Observable.just("루희")
+        justObservable.subscribe { event in
+            print(event)
+        }.disposed(by: disposeBag)
+
+        
+        // of
+        print("of-----------------")
+        let ofObservable = Observable.of(["듀나"])
+        ofObservable.subscribe { event in
+            print(event)
+        }.disposed(by: disposeBag)
+
+        
+        // from
+        print("from-----------------")
+        let fromObservable: Observable<String> = Observable<String>.from(["듀나", "듐보", "후릐"])
+        fromObservable.subscribe { event in
+            print(event)
+        }.disposed(by: disposeBag)
+
+        
+        // empty
+        print("empty-----------------")
+        let emptyObservable = Observable<Void>.empty()
+        emptyObservable.subscribe { event in
+            print(event)
+        }.disposed(by: disposeBag)
+
+        
+        // never
+        print("never-----------------")
+        let neverObservable = Observable<Any>.never()
+        neverObservable.subscribe { event in
+            print(event)
+        }.disposed(by: disposeBag)
+
+        
+        // range
+        print("range-----------------")
+        let rangeObservable = Observable<Int>.range(start: 3, count: 6)
+        rangeObservable.subscribe { event in
+            print(event)
+        }.dispose()
+
+        
+        // repeatElement
+        print("repeatElement-----------------")
+        let repeatElementObservable = Observable<Int>.repeatElement(1)
+        repeatElementObservable.subscribe { event in
+//            print(event)
+        }.disposed(by: disposeBag)
+        
+        
+        // interval
+//        print("interval-----------------")
+//        let intervalObservable = Observable<Int>.interval(RxTimeInterval(2.0), scheduler: MainScheduler.instance)
+//        intervalObservable.subscribe { event in
+//            print(event)
+//        }.disposed(by: disposeBag)
+
+    }
+
+}
+
