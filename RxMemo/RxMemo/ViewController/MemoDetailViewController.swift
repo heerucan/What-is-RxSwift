3c151f017d7051e5baf2f2b83aada5b16d8ed0f0
heerucan <iwpow0307@gmail.com>
2021-10-07 16:11:24 +0900

    [mod] MemoDetail 화면 구현

--- RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
 
     var viewModel: MemoDetailViewModel!
-
+    
+    @IBOutlet weak var listTableView: UITableView!
+    
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
