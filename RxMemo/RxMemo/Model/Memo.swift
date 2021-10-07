933206b22e4d9e433cefd544bd655d81a8e0f808
heerucan <iwpow0307@gmail.com>
2021-10-07 12:40:10 +0900

    [add] Memo Model 추가

---
+++ RxMemo/RxMemo/Model/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by Thisisme Hi on 2021/10/07.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    // 업데이트된 내용으로 새로운 메모 인스턴스를 생성할 때 사용
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
