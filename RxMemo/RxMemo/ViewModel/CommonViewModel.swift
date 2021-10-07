e14c309d7746580620395a29337f9e621f69c609
heerucan <iwpow0307@gmail.com>
2021-10-07 15:50:42 +0900

    [add] MemoComposeViewModel 구현

---
+++ RxMemo/RxMemo/ViewModel/CommonViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CommonViewModel.swift
+//  RxMemo
+//
+//  Created by Thisisme Hi on 2021/10/07.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class CommonViewModel: NSObject {
+    let title: Driver<String>
+    let sceneCoordinator: SceneCoordinatorType
+    let storage: MemoStorageType
+    
+    init(title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.title = Observable.just(title).asDriver(onErrorJustReturn: "")
+        self.sceneCoordinator = sceneCoordinator
+        self.storage = storage
+    }
+}
